b2ed0defee199fb14ad7483cedf9af3470172698
MarcosBurBer <104394729+MarcosBurBer@users.noreply.github.com>
2026-03-02 12:26:50 +0100

    Refactor Prescaler to use new DIV parameter

--- Recuperacion/ADC/Prescaler.vhd
+++ Recuperacion/ADC/Prescaler.vhd
@@ -5,10 +5,10 @@ use IEEE.NUMERIC_STD.ALL;
 entity Prescaler is
     generic ( 
         N_BITS  : integer := 4;  -- Bits necesarios para representar VAL_DIV
-        VAL_DIV : integer := 10  -- Factor de división (Reloj_In / Reloj_Out)
+        DIV : integer := 10  -- Factor de división (Reloj_In / Reloj_Out)
     );
     Port ( 
-        rst     : in  STD_LOGIC;
+        nrst     : in  STD_LOGIC;
         clk_in  : in  STD_LOGIC; -- Reloj de entrada (ej. 200 MHz)
         clk_out : out STD_LOGIC  -- Reloj escalado
     );
@@ -16,31 +16,32 @@ end Prescaler;
 
 architecture Behavioral of Prescaler is
     -- El contador llega hasta (VAL_DIV/2 - 1) para generar un ciclo de trabajo del 50%
-    signal counter : unsigned(N_BITS-1 downto 0) := (others => '0');
-    signal clk_reg : STD_LOGIC := '0';
+    signal s_counter : unsigned(N_BITS-1 downto 0) := (others => '0');
+    signal s_clk_reg : STD_LOGIC := '0';
 begin
 
-    process(clk_in, rst)
+    process(clk_in, nrst)
     begin
-        if rst = '0' then
-            counter <= (others => '0');
-            clk_reg <= '0';
+        if nrst = '0' then
+            s_counter <= (others => '0');
+            s_clk_reg <= '0';
         elsif rising_edge(clk_in) then
             -- Si el divisor es 1, la salida es igual a la entrada (evita división por cero)
-            if VAL_DIV <= 1 then
-                clk_reg <= clk_in;
+            if DIV <= 1 then
+                s_clk_reg <= clk_in;
             else
                 -- Comparamos con la mitad del valor para alternar el estado del reloj
-                if counter >= (VAL_DIV/2 - 1) then
-                    clk_reg <= not clk_reg;
-                    counter <= (others => '0');
+                if s_counter >= (DIV/2 - 1) then
+                    s_clk_reg <= not s_clk_reg;
+                    s_counter <= (others => '0');
                 else
-                    counter <= counter + 1;
+                    s_counter <= s_counter + 1;
                 end if;
             end if;
         end if;
     end process;
 
-    clk_out <= clk_reg;
+    clk_out <= s_clk_reg;
 
 end Behavioral;
+
