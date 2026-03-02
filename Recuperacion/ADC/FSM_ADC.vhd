b484660e8fcebeda803b262867fe58e4e9535559
MarcosBurBer <104394729+MarcosBurBer@users.noreply.github.com>
2026-03-02 11:19:59 +0100

    Clean up FSM_ADC.vhd by removing comments
    Removed commented lines and unnecessary library declarations.

--- Recuperacion/ADC/FSM_ADC.vhd
+++ Recuperacion/ADC/FSM_ADC.vhd
@@ -1,36 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 28.10.2025 15:59:33
--- Design Name: 
--- Module Name: fms - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity FSM_ADC is
   Port (   clk_100 : in STD_LOGIC;
